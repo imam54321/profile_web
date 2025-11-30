d7c8810dd0c6a15af2c645ffda5e400f47067eb0
imam54321 <fauzanimam955gmail.com>
2025-11-30 22:53:34 +0700

    modif background

---
+++ index.js
@@ -0,0 +1,18 @@
+const bgVideo = document.getElementById("bgVideo");
+const audioVideo = document.getElementById("audioVideo");
+
+
+const delay = 3000;
+
+bgVideo.addEventListener('ended', () => {
+    setTimeout(() => {
+        bgVideo.play();
+    }, delay);
+});
+
+audioVideo.addEventListener('ended', () => {
+    setTimeout(() => {
+        audioVideo.play();
+    }, delay);
+});
+
