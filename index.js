ccb3eb37384b6bb4bfed18ee640e9ee050b42913
imam54321 <fauzanimam955gmail.com>
2025-12-11 12:38:06 +0700

    Modif

--- index.js
+++ index.js
@@ -1,22 +1,67 @@
-const bgVideo = document.getElementById("bgVideo");
-const audioVideo = document.getElementById("audioVideo");
+const track = document.querySelector(".carousel-track");
+let slides = document.querySelectorAll(".slide");
+let index = 1;
 
+// clone first & last
+const firstClone = slides[0].cloneNode(true);
+const lastClone = slides[slides.length - 1].cloneNode(true);
 
-const delay = 3000;
+firstClone.id = "first-clone";
+lastClone.id = "last-clone";
 
-bgVideo.addEventListener('ended', () => {
-    setTimeout(() => {
-        bgVideo.play();
-    }, delay);
+track.prepend(lastClone);
+track.append(firstClone);
+
+// update NodeList (karena sudah ada clone baru)
+slides = document.querySelectorAll(".slide");
+
+// posisikan awal di index=1 (slide pertama asli)
+track.style.transform = `translateX(-100%)`;
+
+// fungsi pindah slide
+function moveToSlide() {
+  track.style.transition = "transform 0.5s ease";
+  track.style.transform = `translateX(-${index * 100}%)`;
+}
+
+// hapus efek lompat ketika kena clone
+track.addEventListener("transitionend", () => {
+  if (slides[index].id === "first-clone") {
+    track.style.transition = "none";
+    index = 1;
+    track.style.transform = `translateX(-100%)`;
+  }
+  if (slides[index].id === "last-clone") {
+    track.style.transition = "none";
+    index = slides.length - 2;
+    track.style.transform = `translateX(-${index * 100}%)`;
+  }
 });
 
-audioVideo.addEventListener('ended', () => {
-    setTimeout(() => {
-        audioVideo.play();
-    }, delay);
+// tombol
+document.querySelector(".next").addEventListener("click", () => {
+  index++;
+  moveToSlide();
+  pauseAutoSlide();
 });
 
-document.addEventListener("click", () => {
-    video.muted = false ;
-    video.play();
+document.querySelector(".prev").addEventListener("click", () => {
+  index--;
+  moveToSlide();
+  pauseAutoSlide();
 });
+
+// autoplay
+let autoSlide;
+function startAutoSlide() {
+  autoSlide = setInterval(() => {
+    index++;
+    moveToSlide();
+  }, 4000);
+}
+function pauseAutoSlide() {
+  clearInterval(autoSlide);
+  setTimeout(startAutoSlide, 7000);
+}
+
+startAutoSlide();
